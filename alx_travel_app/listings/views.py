d3c6cc8a9538ffe15ef5dd6cf90767b1d84baf2c
Maha065 <maha060506@gmail.com>
2025-10-01 18:33:18 +0100

    Create views.py

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Listings to be viewed or edited.
+    Provides CRUD operations.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Bookings to be viewed or edited.
+    Provides CRUD operations.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
